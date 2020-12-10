b79290dae466ac806f974c5e57beafde7091d652
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2020-12-09 18:37:43 -0800

    aboutme

--- puppyland/main/views.py
+++ puppyland/main/views.py
@@ -4,3 +4,5 @@ from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def aboutus(request):
+    return render(request,'aboutus.html')
