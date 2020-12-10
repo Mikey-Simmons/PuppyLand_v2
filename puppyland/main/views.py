c21aea49e685775b068235ded9093686a6f64765
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2020-12-09 17:42:54 -0800

    added home page and style

---
+++ puppyland/main/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Dog, User
+from django.contrib import messages
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
